8f1bb92939d8b47850318daf5e8dd4143e62a41c
Debaditya Dutta <dolphin.ned@gmail.com>
2023-07-30 15:05:38 -0500

    working version

--- openai_django/base_app/views.py
+++ openai_django/base_app/views.py
@@ -14,6 +14,10 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
 
 
 os.environ["OPENAI_API_KEY"] = settings.OPENAI_API_KEY
@@ -23,21 +27,21 @@ def query_view(request):
     if request.method == 'POST':
         prompt = request.POST.get('prompt')
 
-        loader = DirectoryLoader("data/")
-        documents = loader.load()
-        # split the documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.split_documents(documents)
-        # select which embeddings we want to use
-        embeddings = OpenAIEmbeddings()
-        # create the vectorestore to use as the index
-        db = Chroma.from_documents(texts, embeddings)
-        # expose this index in a retriever interface
-        retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
-        # create a chain to answer questions 
-        qa = ConversationalRetrievalChain.from_llm(OpenAI(), retriever)
-        chat_history = getHistory( request )
-        result = qa({"question": prompt, "chat_history": chat_history})
+        if os.path.exists("persist"):
+            print("Reusing index...\n")
+            vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+            index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+        else:
+            #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+            loader = DirectoryLoader("data/")
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+         
+        chain = ConversationalRetrievalChain.from_llm(
+                llm=ChatOpenAI(model="gpt-3.5-turbo"),
+                retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+                )
+        
+        result = chain( { "question": prompt, "chat_history": listToTuple( getHistory( request ) ) } )
         answer = result['answer']
 
         appendToHistory( request, prompt, answer )
@@ -48,13 +52,20 @@ def query_view(request):
 def appendToHistory( request, prompt, answer ):
     request.session["history"].append( ( prompt, answer ) )
     request.session["history"] = request.session["history"][-10:]    
-    # print( request.session["history"] )
+    print( request.session["history"] )
     request.session.modified = True
 
     
 def getHistory( request ):
-    request.session["history"] = []
+    # request.session["history"] = []
+    # request.session.modified = True
     if "history" not in request.session:
         request.session["history"] = []
         request.session.modified = True
     return request.session["history"]
+
+def listToTuple( x ):
+    y = []
+    for item in x:
+        y.append( tuple( item ) )
+    return y
